df22652d973e5a64f2a5b4991b540c4776b74dc2
testedcode <abhi86813@gmail.com>
2025-12-07 21:56:17 +0530

    logo add and Navbar.jsx updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
-import "../styles/Navbar.css";
+import "./Navbar.css";
 
 const Navbar = () => {
   const location = useLocation();
